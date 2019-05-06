c1981e97706439477aa89d61f9e00345884926d7
ogendogen <kosmos500@interia.pl>
2019-05-06 21:53:09 +0200

    Attempt to data clearing

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello world")
+import time
+import datetime
+import os
+
+# clears data by removing interpunction, digits, line endings, redudant spaces and converting to lower case
+def clearData(data): # need to pass language name in parameter to check custom interpunction
+    standardInterpunction = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?", "*"]
+    digits = "1234567890"
+    customInterpunction = list("todo")
+
+    outputData = ""
+    for literal in data:
+        if not isinstance(literal, str):
+            continue
+        if literal in standardInterpunction:
+            continue
+        if literal in customInterpunction:
+            continue
+        if literal in digits:
+            continue
+        if literal == "\n" or literal == "r":
+            outputData = outputData + " "
+
+        outputData = outputData + literal.lower()
+
+    return outputData
+
+try:
+    # loading up files and clearing data
+    filesList = os.listdir("text samples")
+    for fileName in filesList:
+        f = open("text samples/" + fileName, "r", encoding="utf8")
+        fileContent = f.read().encode("utf-8")
+        f.close()
+        clearedData = clearData(fileContent)
+        # print("clear data")
+
+        f = open("cleared data/" + fileName, "w")
+        f.write(clearedData)
+        f.close()
+    
+    print("Finished succesfully!")
+
+except Exception as ex:
+    time = time.time()
+    timestamp = datetime.datetime.fromtimestamp(time).strftime('%Y%m%d%H%M%S')
+    logFile = open("error logs/errorlog" + timestamp + ".log", "w")
+    logFile.write(str(ex))
+    logFile.close()
+    print("Error: " + str(ex))
